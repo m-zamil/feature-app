f991c10d0d347d8ce0baecb0ef7b9427358c2be3
Muzamil Khan <88248071+m-zamil@users.noreply.github.com>
2021-09-17 11:54:39 +0500

    meet and greet

--- js/index.js
+++ js/index.js
@@ -1,13 +1,11 @@
 function rudrSwitchTab(rudr_tab_id, rudr_tab_content) {
-  // first of all we get all tab content blocks (I think the best way to get them by class names)
   var x = document.getElementsByClassName("tabcontent");
   var i;
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; // hide all tab content
+    x[i].style.display = "none";
   }
-  document.getElementById(rudr_tab_content).style.display = "block"; // display the content of the tab we need
+  document.getElementById(rudr_tab_content).style.display = "block";
 
-  // now we get all tab menu items by class names (use the next code only if you need to highlight current tab)
   var x = document.getElementsByClassName("tabmenu");
   var i;
   for (i = 0; i < x.length; i++) {
